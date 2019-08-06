c7d8e8b0ffc5d428d84a0f6df1c4ff3320bc8a00
larynxmith <larynxmith@gmail.com>
2019-08-06 16:54:08 -0700

    preGame pahse complete

--- js/newscript.js
+++ js/newscript.js
@@ -4,64 +4,14 @@ let cardsInCrib = 0;
 let lowerHand;
 let upperHand;
 let deck;
+let poneHand;
+let dealerHand;
 //Deal Phase
 
 
-
-
-// });
-
-console.log("Crib Hand: " + cribHand);
-console.log("Lower Hand: " + lowerHand);
-console.log("Upper Hand: " + cribHand);
             
 
-//var isCardSelected = false;
 
-    // cribHand.addCard(card);
-    //     console.log("Crib Hand: " + cribHand);
-    //     cribHand.render();
-
-    // isCardSelected = !isCardSelected;
-    // if (isCardSelected) {
-    //     document.getElementById("p1-button").style.visibility = "visible";
-    // }
-    // document.getElementById("p1-button").addEventListener("click", function(card) {
-    //     console.log(card);
-    //     cribHand.addCard(card);
-    //     console.log("Crib Hand: " + cribHand);
-    //     //cribHand.render();
-    // })
-    
-    // console.log(isCardSelected);
-    // console.log(lowerHand);
-    //  card.moveTo(card.targetLeft + 34.5, card.targetTop + 47 - 25  , 500);
-    //     lowerHand.render();
-
-
-
-
-//functions
-
-// function createCrib () {
-//     cribHand = new cards.Hand({faceUp: false});
-//     cribHand.x -= 300;
-//     cribHand.render({callback:function() {
-// 	cribHand.addCard(deck.topCard());
-//     cribHand.render();
-//     console.log("Crib Hand: " + cribHand);
-
-// }});
-// }
-
-// function dealHands () {
-//     upperHand = new cards.Hand({faceUp: false, y:50});
-//     lowerHand = new cards.Hand({faceUp: true, y:600});
-
-    // deck.deal(6, [upperHand, lowerHand], 50);
-    // console.log("Lower Hand: " + lowerHand);
-    // console.log("Upper Hand: " + upperHand);
-// }
 $( document ).ready(function() {
     console.log( "ready!" );
     startGame();
@@ -71,57 +21,68 @@ $( document ).ready(function() {
 
 function startGame() {
     cribHand = [];
+    
     //create deck
     cards.init({table:"#game-table"});
     deck = new cards.Deck();
     deck.addCards(cards.all)
     deck.render({immediate:true});
     ///
-    document.getElementById("gb-button").addEventListener("click", function() {
-        //create hands
-        upperHand = new cards.Hand({faceUp: false, y:50});
-        lowerHand = new cards.Hand({faceUp: true, y:600});
-        deck.deal(6, [upperHand, lowerHand], 50);
-        //create crib
-        createCrib();
-        //deal to  hands
-        preGame(lowerHand, function() { preGame(upperHand) });
-
+    document.getElementById("gb-button").addEventListener("click", onStartClick);
+}
+function onStartClick() {
+    //create hands
+    upperHand = new cards.Hand({faceUp: false, y:50});
+    lowerHand = new cards.Hand({faceUp: true, y:600});
+    deck.deal(6, [upperHand, lowerHand], 50);
+    poneHand = lowerHand;
+    dealerHand = upperHand;
+    //create crib
+    createCrib();
+    //deal to hands
+    console.log(lowerHand);
+    preGame(lowerHand, function() {
+        preGame(upperHand) 
     });
 }
 
+//Pre Game Phase
 
 function preGame(currentHand, cb) {
     //STRETCH--offer cut to select Dealer, reshuffle deck, assign Dealer
 
     //add listener for current hand
     let currentCard;
-    currentHand.click(function(card) {
-        console.log(card)
+    currentHand.click(onCardClick);
+    function onCardClick(card) {
         currentCard = card;
-        document.getElementById("crib-button").style.visibility = "visible";
-    });
-
-    document.getElementById("crib-button").addEventListener("click", () => {
+        if (cardsInCrib < 4) {
+            document.getElementById("crib-button").style.visibility = "visible";
+        }
+     }
+    document.getElementById("crib-button").addEventListener("click", onCribButtonClick)
+    
+    function onCribButtonClick() {
         // currentHand.faceUp = false;
         document.getElementById("crib-button").style.visibility = "hidden";
-        addCribCards(currentCard) 
+        addCribCards(currentCard); 
 
         if (cardsInCrib === 2) {
-            console.log("hello cards in crib");
-            
-            if(cb == undefined) { 
-                return; 
-            } else {
-                console.log("Your Turn is Over; switch to dealer");
-                console.log(cb);
-                cb()
-            };
-        }
-    });
+            changeHandVisibility(); 
+            //$(currentHand).off("click", onCardClick);
 
-    
-    
+            document.getElementById("crib-button").removeEventListener("click", onCribButtonClick)
+            //currentHand.unbind("click");
+            console.log("Your Turn is Over; switch to dealer");
+            cb();
+        } else if(cardsInCrib === 4) {
+            document.getElementById("crib-button").removeEventListener("click", onCribButtonClick)
+            document.getElementById("gb-button").removeEventListener("click", onStartClick);
+            document.getElementById("gb-button").textContent = "Let's Begin!"
+            playPhase();
+            
+        }
+    }
 }
 
 function createCrib() {
@@ -129,18 +90,34 @@ function createCrib() {
     cribHand.x -= 260;
     cribHand.render({callback:function() {
         cribHand.render();
-        console.log("Crib Hand: " + cribHand);
     }});
 }
 function addCribCards(card, cb) {
     cribHand.addCard(card);
-    console.log("Crib Hand: " + cribHand);
     cribHand.render();
     cardsInCrib++
     document.getElementById("crib-button").removeEventListener("click", addCribCards);
-    console.log("Cards in Crib: " + cardsInCrib);
         
 }
+
+
+function changeHandVisibility() {
+    poneHand.faceUp = !poneHand.faceUp;
+    dealerHand.faceUp = !dealerHand.faceUp;
+    poneHand.render();
+    dealerHand.render();
+
+}
+function switchRoles() {
+    if (poneHand === lowerHand) {
+        dealerHand = lowerHand;
+        poneHand = upperHand;
+    } else
+        dealerHand = upperHand;
+        poneHand = lowerHand;
+}
+
+// Play Phase
 function createGameBoard() {
     gameBoard = new cards.Hand({faceUp: true});
     gameBoard.x += 50;
@@ -151,17 +128,6 @@ function createGameBoard() {
 function playPhase() {
     createGameBoard();
     //shift deck over to give Play space
-    deck.click(function(card) {
         deck.x -= 140;
         deck.render();
-    })
 }
-
-//         if (cardsInCrib = 2) {
-//             // lowerHand.off("click");
-//             if(cb == undefined) {
-//                 return; 
-//             } else {
-//                 cb()
-//             };
-//         }
