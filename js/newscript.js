01f3825f4fe6eb3f48f03aafa1fdd36b51ba0d6b
larynxmith <larynxmith@gmail.com>
2019-08-08 03:07:39 -0700

    Play Phase operational through Reset with Winner posted

--- js/newscript.js
+++ js/newscript.js
@@ -10,6 +10,7 @@ let playerTwoMatchPoints= 0;
 let ponePoints = 0;
 let dealerPoints = 0;
 let gameBoardCards = 0;
+let pointsToWin = 121
 let lowerHand;
 let upperHand;
 let deck;
@@ -54,7 +55,7 @@ function onStartClick() {
     poneHand = lowerHand;
     dealerHand = upperHand;
     document.getElementById("dealer-top").style.visibility = "visible";
-    document.getElementById("message-board").textContent = "Pone, please select two cards, one at a time, to send to the Dealer's Crib and Press the 'Send to Crib?' Button";
+    document.getElementById("message-board").textContent = "Pone, please select two cards, one at a time, to send to the Dealer's Crib and Press the 'Send to Crib?' Button for Each.";
     isPoneTurn = true;
     //create crib
     createCrib();
@@ -99,7 +100,7 @@ function preGame(currentHand, cb) {
             document.getElementById("crib-button").removeEventListener("click", onCribButtonClick)
             //currentHand.unbind("click");
             console.log("Your Turn is Over; switch to dealer");
-            document.getElementById("message-board").textContent = "Dealer, please select two cards, one at a time, to send to Your Crib and Press the 'Send to Crib?' Button";
+            document.getElementById("message-board").textContent = "Dealer, please select two cards, one at a time, to send to Your Crib and Press the 'Send to Crib?' Button for Each.";
             cb();
         } else if(cardsInCrib === 4) {
             //console.log("hi");
@@ -155,12 +156,6 @@ function playPhase() {
     createGameBoard();
     showTopCard();
     addGameCards();
-    //playCardsOnBoard();
-    if (roundCount > 21 && roundCount < 31) {
-        console.log("Checking for Go");
-        currentPlayerPoints = checkForGo(currentPlayerPoints);
-    };
-    
 }
 function createGameBoard() {
     gameBoard = new cards.Hand({faceUp: true});
@@ -189,7 +184,7 @@ function showTopCard () {
 }});
 }
 function addGameCards (card) {
-    document.getElementById("message-board").textContent = "Pone, please play a card";
+    document.getElementById("message-board").textContent = "Let's Play! Pone, You're Up!";
 }
 function handleHandClick (card) {
     if (currentPhase === 0) {
@@ -222,12 +217,7 @@ function handleHandClick (card) {
             endGame();
         }
     }
-    console.log("IS it pone's turn?" + isPoneTurn)
 }
-// function playCardsOnBoard (card) {
-//     poneHandClick(card);
-//     dealerHandClick(card);
-// }
 
 function poneHandClick (card) {
     gameBoard.addCard(card);
@@ -235,52 +225,50 @@ function poneHandClick (card) {
     gameBoard.render();
     updateRoundCount(card);
     ponePoints = playPointsCheck(ponePoints);
-    console.log("Player One's points: " + ponePoints);
-    //getElementById("round-count").textContent = roundCount.value;
-    //console.log( roundCount);
     changeHandVisibility();
-    document.getElementById("message-board").textContent = "Dealer, please play a card";
+    //document.getElementById("message-board").textContent = "Dealer, please play a card";
 
 }
 function dealerHandClick(card){
     gameBoard.addCard(card);
-    //card.y -= 25;
     gameBoard.render();
     updateRoundCount(card);
     dealerPoints = playPointsCheck(dealerPoints);
-    console.log("Player Two's points: " + dealerPoints);
-    //
     changeHandVisibility();
-    document.getElementById("message-board").textContent = "Pone, please play a card";
+    //document.getElementById("message-board").textContent = "Pone, please play a card";
 
 }
 function updateRoundCount (card) {
     let value = Math.min((card.rank % 14), 10)
-    console.log("Card rank: " + card.rank +" new value = " + value);
     roundCount += value;
     console.log("round Count = " + roundCount);
-    document.getElementById("round-count").textContent = roundCount;
     document.getElementById("pone-points").textContent = ponePoints;
     document.getElementById("dealer-points").textContent = dealerPoints;
+    document.getElementById("round-count").textContent = roundCount;
     return roundCount;
 }
 
 
 function playPointsCheck (currentPlayerPoints) {
     switch (true) {
+        // case (roundCount > 21 && roundCount < 31) :
+        //     console.log("Checking for Go");
+        //     currentPlayerPoints = checkForGo(currentPlayerPoints);
+        
         case (gameBoard.length === 2) :
+            console.log("hi2");
             currentPlayerPoints = checkforFifteen(currentPlayerPoints);
-            console.log(currentPlayerPoints);
             currentPlayerPoints = checkForPair(currentPlayerPoints);
-            console.log(currentPlayerPoints);
             break;
         case (gameBoard.length === 3) :
+            console.log("hi3");
             currentPlayerPoints = checkforFifteen(currentPlayerPoints);
             currentPlayerPoints = checkForPair(currentPlayerPoints);
             currentPlayerPoints = checkForRoyal(currentPlayerPoints);
             currentPlayerPoints = checkForRuns(currentPlayerPoints);
             break;
         case (gameBoard.length >= 4) :
+            console.log("hi4");
             currentPlayerPoints = checkforFifteen(currentPlayerPoints);
             currentPlayerPoints = checkForPair(currentPlayerPoints);
             currentPlayerPoints = checkForRoyal(currentPlayerPoints);
@@ -297,37 +285,31 @@ function playPointsCheck (currentPlayerPoints) {
 
 function checkforFifteen (currentPlayerPoints) {
     if(roundCount === 15) {
-        console.log("Fifteen for Two");
+        document.getElementById("message-board").textContent = "Fifteen for Two";
         return currentPlayerPoints + 2;
     }
     return currentPlayerPoints;
 };
 function checkForPair (currentPlayerPoints) {
     let i = gameBoard.length - 1;
-    console.log(gameBoard);
-    console.log("Current card is : " + gameBoard[i]);
     if (gameBoard[i].rank === gameBoard[i-1].rank) {
-        console.log("Pair for Two");
+        document.getElementById("message-board").textContent = "Pair for Two";
         return currentPlayerPoints + 2;
     }
     return currentPlayerPoints;
 }
 function checkForRoyal (currentPlayerPoints) {
     let i = gameBoard.length - 1;
-    console.log(gameBoard);
-    console.log("Current card is : " + gameBoard[i]);
     if (gameBoard[i].rank === gameBoard[i-1].rank && gameBoard[i-1].rank === gameBoard[i-2].rank) {
-        console.log("Pair Royal for Six"); 
+        document.getElementById("message-board").textContent = "Pair Royal for Six"; 
         return currentPlayerPoints + 4;
     }
     return currentPlayerPoints;
 }
 function checkForDoubleRoyal (currentPlayerPoints) {
     let i = gameBoard.length - 1;
-    console.log(gameBoard);
-    console.log("Current card is : " + gameBoard[i]);
     if (gameBoard[i].rank === gameBoard[i-1].rank && gameBoard[i-1].rank === gameBoard[i-2].rank && gameBoard[i-2].rank === gameBoard[i-3].rank) {
-        console.log("Double Pair Royal for Twelve");
+        document.getElementById("message-board").textContent = "Double Pair Royal for Twelve";
         return currentPlayerPoints +=6 ;
     }
     return currentPlayerPoints;
@@ -338,27 +320,75 @@ function checkForRuns(currentPlayerPoints) {
 }
 function checkForThirtyOne(currentPlayerPoints) {
     if (roundCount === 31) {
+        document.getElementById("message-board").textContent = "Thirty-One for Two; Count is Reset";
+        roundCount = 0;
         return currentPlayerPoints + 2
     }
     return currentPlayerPoints;
 }
 function checkForGo () {
-    currentPlayerHand.forEach((card) => {
-        if (roundCount + card.rank <= 31) {
-            return;
-        } else {
-        //declare a Go for current player, and check for Go for other player, allowing them to play up to 31
-        //assign +1 for last card played without reaching 31, or +2 for reaching 31
-        //reset roundCount, resume play
-        };
-    });
+    if(isPoneTurn) {
+        poneHand.forEach((card) => {
+        
+            if (roundCount + card.rank <= 31) {
+                console.log("Card to play?" + (roundCount + card.rank));
+                return;
+            } else {
+                console.log("next step");
+                // dealerHand.forEach((card) => {
+                // if (roundCount > 21 && roundCount < 31) {
+                //     console.log("Checking for Go");
+                //     currentPlayerPoints = checkForGo(currentPlayerPoints);
+                // };
+            //declare a Go for current player, and check for Go for other player, allowing them to play up to 31
+            //assign +1 for last card played without reaching 31, or +2 for reaching 31
+            //reset roundCount, resume play
+            };
+        });
+        return currentPlayerPoints;
+    }
 }
 function endGame(){
-    currentPhase = 2
+    currentPhase = 2;
     playerOneGamePoints = ponePoints;
     playerTwoGamePoints = dealerPoints;
+    if (playerOneGamePoints > playerTwoGamePoints) {
+        document.getElementById("message-board").textContent = "Player One Wins!!! Click the 'Play Another Round' Button to Try Again!"
+    } else if (playerTwoGamePoints > playerOneGamePoints) {
+        document.getElementById("message-board").textContent = "Player Two Wins!!! Click the 'Play Another Round' Button to Try Again!"
+    } else {
+        document.getElementById("message-board").textContent = "Uh... a TIE?!?!?! Don't worry, Click the 'Play Another Round' Button to Try Again!"
+    }
+
     document.getElementById("p2-game-points").textContent = playerTwoGamePoints;
     document.getElementById("p1-game-points").textContent = playerOneGamePoints;
-    //TBD
+    document.getElementById("crib-button").style.visibility = "visible";
+    document.getElementById("crib-button").textContent = "Play Another Round?";
+    document.getElementById("crib-button").addEventListener("click", clearVariables);
+
+    function clearVariables() {
+        ponePoints = 0;
+        dealerPoints = 0;
+        roundCount = 0;
+        console.log(roundCount);
+        currentPlayerPoints = 0;
+        cardsInCrib = 0;
+        poneHand = [];
+        dealerHand = [];
+        cribHand = [];
+        gameBoard = [];
+        document.getElementById("gb-button").style.visibility = "visible";
+        document.getElementById("crib-button").textContent = "Play Another Round?";
+        document.getElementById("crib-button").addEventListener("click", clearVariables);
+        document.getElementById("crib-button").removeEventListener("click", clearVariables);
+        document.getElementById("crib-button").style.visibility = "hidden";
+        document.getElementById("crib-button").textContent = "Send to Crib?";
+        document.getElementById("pone-points").textContent = ponePoints;
+        document.getElementById("dealer-points").textContent = dealerPoints;
+        document.getElementById("round-count").textContent = roundCount;
+        switchRoles();
+        startGame();
+
+    };
 }
 
